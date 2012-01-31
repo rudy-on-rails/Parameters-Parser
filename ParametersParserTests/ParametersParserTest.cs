473b5f7ea4f3cbaaf9fdf00124372a02f721ad00
Rudy Wagner Seidinger Jr <rudyseidinger@gmail.com>
2012-01-31 10:00:18 -0200

    updating test methods to a common pattern...

--- ParametersParserTests/ParametersParserTest.cs
+++ ParametersParserTests/ParametersParserTest.cs
@@ -8,40 +8,40 @@ namespace ParametersParserTests {
     [TestClass]
     public class ParametersParserTest {
         [TestMethod]
-        public void ParametersParserTest_BooleanValue_shouldReturnTrueIfParameterIsPresent() {
+        public void ParametersParserTest_BooleanValue_shouldReturnTrue_IfParameterIsPresent() {
             string[] parameters = new string[] {"-p"};
             Assert.IsTrue(new ParameterParser(parameters).getParameterValue("-p").BooleanValue());
         }
 
         [TestMethod]
-        public void ParametersParserTest_BooleanValue_shouldReturnFalseIfParameterIsNotPresent() {
+        public void ParametersParserTest_BooleanValue_shouldReturnFalse_IfParameterValueIsNotPresent() {
             string[] parameters = new string[] { "-p" };
             Assert.IsFalse(new ParameterParser(parameters).getParameterValue("-x").BooleanValue());
         }
 
         [TestMethod]
-        public void ParametersParserTest_IntegerValue_shouldReturnParamterAsIntIfIsValid() {
+        public void ParametersParserTest_IntegerValue_shouldReturnParamterAsInt_IfIsValid() {
             string[] parameters = new string[] { "-p", "1220" };
             Assert.AreEqual(1220, new ParameterParser(parameters).getParameterValue("-p").IntegerValue());
         }
 
         [ExpectedException(typeof(ParameterException))]
         [TestMethod]
-        public void ParametersParserTest_IntegerValue_shouldThrownParameterExceptionIntIfIsNotPresent() {
+        public void ParametersParserTest_IntegerValue_shouldThrownParameterException_IfParameterValueIsNotPresent() {
             string[] parameters = new string[] { "-p" };
             new ParameterParser(parameters).getParameterValue("-p").IntegerValue();
         }
 
         [ExpectedException(typeof(ParameterException))]
         [TestMethod]
-        public void ParametersParserTest_IntegerValue_shouldThrownParameterExceptionIntIfIsNotValid() {
+        public void ParametersParserTest_IntegerValue_shouldThrownParameterExceptionInt_IfParameterValueIsNotValid() {
             string[] parameters = new string[] { "-p", "ssss" };
             new ParameterParser(parameters).getParameterValue("-p").IntegerValue();
         }
 
         [ExpectedException(typeof(ParameterNotFoundException))]
         [TestMethod]
-        public void ParametersParserTest_IntegerValue_shouldThrownParameterNotFoundExceptionInt_IfParamIsNotPresent() {
+        public void ParametersParserTest_IntegerValue_shouldThrownParameterNotFoundExceptionInt_IfParameterIsNotPresentAndIsRequired() {
             string[] parameters = new string[] { "-x", "ssss" };
             new ParameterParser(parameters).getParameterValue("-p", true).IntegerValue();
         }
